6ab2fea8492d9dada4162e57e217d1a896ccabed
raed1236 <raed.almangour@gmail.com>
2023-11-30 05:28:07 +0300

    حذف

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/data/WalletContext.kt
+++
@@ -1,8 +0,0 @@
-package fr.acinq.phoenix.data
-
-/** Contains contextual information for the wallet, fetched from https://acinq.co/phoenix/walletcontext.json. */
-data class WalletContext(
-    val isMempoolFull: Boolean,
-    val androidLatestVersion: Int,
-    val androidLatestCriticalVersion: Int,
-)
