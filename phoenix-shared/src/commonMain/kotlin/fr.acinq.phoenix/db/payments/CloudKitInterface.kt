eb7460e9283ca16d00dbb0d6f4074efab6d5e1e1
رايد <raed.almangour@gmail.com>
2024-03-15 10:33:00 +0300

    Delete phoenix-shared directory

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/db/payments/CloudKitInterface.kt
+++
@@ -1,5 +0,0 @@
-package fr.acinq.phoenix.db.payments
-
-/* Cross-platform placeholder for CloudKitDb. */
-interface CloudKitInterface {
-}
--- phoenix-shared/src/iosMain/kotlin/fr/acinq/phoenix/data/iosElectrumRegtestConf.kt
+++
@@ -1,6 +0,0 @@
-package fr.acinq.phoenix.data
-
-import fr.acinq.lightning.io.TcpSocket
-import fr.acinq.lightning.utils.ServerAddress
-
-actual fun platformElectrumRegtestConf(): ServerAddress = ServerAddress(host = "127.0.0.1", port = 51002, TcpSocket.TLS.DISABLED)
